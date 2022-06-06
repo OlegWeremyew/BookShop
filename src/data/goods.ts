8e4100bcb41b8dfb69d0cd5a8908543c4f114456
olegweremyew <olegweremey1994@mail.ru>
2022-06-06 15:54:11 +0300

    added new books

--- src/data/goods.ts
+++ src/data/goods.ts
@@ -3,7 +3,7 @@ import { GoodsType } from './types';
 export const goods: GoodsType[] = [
   {
     id: 'js01',
-    category: 'books',
+    category: 'education',
     name: 'Дэвид Флэнаган. JavaScript Карманный справочник',
     poster:
       'https://images.unsplash.com/photo-1543002588-bfa74002ed7e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
@@ -11,7 +11,7 @@ export const goods: GoodsType[] = [
   },
   {
     id: 'js02',
-    category: 'books',
+    category: 'education',
     name: 'Дэвид Флэнаган. JavaScript Подробное руководство',
     poster:
       'https://images.unsplash.com/photo-1532012197267-da84d127e765?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
@@ -19,7 +19,7 @@ export const goods: GoodsType[] = [
   },
   {
     id: 'js03',
-    category: 'books',
+    category: 'education',
     name: 'Алекс Бэнкс и Ева Порселло. React и Redux',
     poster:
       'https://images.unsplash.com/photo-1544716278-ca5e3f4abd8c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80',
@@ -27,7 +27,7 @@ export const goods: GoodsType[] = [
   },
   {
     id: 'data01',
-    category: 'books',
+    category: 'education',
     name: 'Алекс Бэнкс и Ева Порселло. GraphQL',
     poster:
       'https://images.unsplash.com/photo-1532012197267-da84d127e765?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80',
@@ -35,7 +35,7 @@ export const goods: GoodsType[] = [
   },
   {
     id: 'js04',
-    category: 'books',
+    category: 'education',
     name: 'Робин Вирух. Путь к изучению React',
     poster:
       'https://images.unsplash.com/photo-1589998059171-988d887df646?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=755&q=80',
@@ -43,15 +43,15 @@ export const goods: GoodsType[] = [
   },
   {
     id: 'video01',
-    category: 'video',
-    name: 'Фундаментальный JavaScript',
+    category: 'education',
+    name: 'Фундаментальный JavaScript. Основы языка',
     poster:
       'https://images.unsplash.com/photo-1536240478700-b869070f9279?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=862&q=80',
     price: 1250,
   },
   {
     id: 'video02',
-    category: 'video',
+    category: 'education',
     name: 'Анимации в JavaScript',
     poster:
       'https://images.unsplash.com/photo-1492619375914-88005aa9e8fb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=746&q=80',
@@ -59,7 +59,7 @@ export const goods: GoodsType[] = [
   },
   {
     id: 'video03',
-    category: 'video',
+    category: 'education',
     name: 'Основы TypeScript',
     poster:
       'https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=740&q=80',
@@ -67,7 +67,7 @@ export const goods: GoodsType[] = [
   },
   {
     id: 'video04',
-    category: 'video',
+    category: 'education',
     name: 'Pro React',
     poster:
       'https://images.unsplash.com/photo-1550784343-6bd0ce5d600b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80',
@@ -121,7 +121,7 @@ export const goods: GoodsType[] = [
   },
   {
     id: 'AgataKristy01',
-    category: 'detectives01',
+    category: 'detectives',
     name: 'Печальный кепарис',
     poster:
       'https://cv3.litres.ru/pub/c/elektronnaya-kniga/cover_415/56080435-agata-kristi-pechalnyy-kiparis-56080435.jpg',
@@ -129,7 +129,7 @@ export const goods: GoodsType[] = [
   },
   {
     id: 'AgataKristy02',
-    category: 'detectives02',
+    category: 'detectives',
     name: 'Спящий убийца',
     poster:
       'https://cv8.litres.ru/pub/c/audiokniga/cover_415/65397586-agata-kristi-spyaschiy-ubiyca-65397586.jpg',
@@ -137,31 +137,169 @@ export const goods: GoodsType[] = [
   },
   {
     id: 'AgataKristy03',
-    category: 'detectives03',
+    category: 'detectives',
     name: 'Труп в библиотеке',
     poster: 'https://s3-goods.ozstatic.by/1000/854/61/101/101061854_0.jpg',
     price: 900,
   },
   {
     id: 'GeorgeMartin',
-    category: 'fantasyAndHistorical01',
+    category: 'novels',
     name: 'Сборник книг',
     poster: 'https://img.labirint.ru/images/att/news/1-17637-1504101837-2516.jpg',
     price: 1900,
   },
   {
     id: 'science01',
-    category: 'kidsBook',
+    category: 'forKids',
     name: 'Детская книга про динозавров',
     poster:
       'https://irecommend.ru/sites/default/files/imagecache/copyright1/user-images/170947/lqOrmXUMovjRGcAILvxvXA.jpg',
     price: 350,
   },
   {
-    id: ' ErnestMillerHemingway01',
-    category: 'kidsBook',
+    id: 'ErnestMillerHemingway01',
+    category: 'forKids',
     name: 'Старик и море',
     poster: 'https://briefly.ru/static/cache/illustrations/720/8.jpeg?1579589779',
     price: 850,
+  },
+  {
+    id: 'ArthurConanDoyle01',
+    category: 'autobiographies',
+    name: 'Сэр Артур Конан Дойл',
+    poster:
+      'https://s26162.pcdn.co/wp-content/uploads/sites/3/2020/06/arthur-conan-doyle-no-es-va-lucrar-tant-com-els-seus-plagiadors-del-feno-men-de-sherlock-holmes.jpeg',
+    price: 1250,
+  },
+  {
+    id: 'ArthurConanDoyle02',
+    category: 'detectives',
+    name: 'Приключения Шерлока Холмса: сборник',
+    poster:
+      'https://bashenka.by/20947-thickbox_default/knigaepokhapriklyucheniya-sherloka-kholmsbumazhnaya-oblozhka-artur-konan-dojl.jpg',
+    price: 1250,
+  },
+  {
+    id: 'ArthurConanDoyle03',
+    category: 'detectives',
+    name: 'Приключения Шерлока Холмса: "Этюд в розовых тонах"',
+    poster:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTG19CaSXITvl430eFgDsM5an13A58gn4v5Ig&usqp=CAU',
+    price: 1170,
+  },
+  {
+    id: 'ArthurConanDoyle04',
+    category: 'adventures',
+    name: 'Путешествие к центру земли, книга приключений',
+    poster: 'https://storytel.com/images/640x640/0000056676.jpg',
+    price: 1170,
+  },
+  {
+    id: 'TailsBook01',
+    category: 'forKids',
+    name: 'Сбоник детских сказок и рассказов',
+    poster: 'https://jili-blog.ru/wp-content/uploads/2016/04/21_sapgir_razv2.jpg',
+    price: 470,
+  },
+  {
+    id: 'TailsBook02',
+    category: 'forKids',
+    name: 'Русская народная сказка: "Иван царевич и серый волк"',
+    poster:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRenw9Opvu3i6YldC8sU-MdvawI0etv1WkfAQ&usqp=CAU',
+    price: 720,
+  },
+  {
+    id: 'TailsBook03',
+    category: 'forKids',
+    name: 'Русская народная сказка: "Конёк горбунок"',
+    poster: 'https://static.kulturologia.ru/files/u23581/chudo-judo-ryba-kit.jpeg',
+    price: 565,
+  },
+  {
+    id: 'TailsBook04',
+    category: 'forKids',
+    name: 'Русская народная сказка: "Чудо чудное, диво дивное"',
+    poster:
+      'https://knizhka-malishka.ru/wp-content/uploads/2019/02/Divo_divnoe_chudo_chudnoe_russkaya_narodnaya_skazka-9.jpg',
+    price: 460,
+  },
+  {
+    id: 'TailsBook05',
+    category: 'forKids',
+    name: 'Русская народная сказка: "Мужик и медведь"',
+    poster: 'https://www.kostyor.ru/images0/imagestales/t92_1.jpg',
+    price: 510,
+  },
+  {
+    id: 'TailsBook06',
+    category: 'forKids',
+    name: 'Русская народная сказка: "Царевна лягушка"',
+    poster:
+      'https://kratkoe.com/wp-content/uploads/2021/09/tsarevna-lyagushka-voprosy-i-otvety-k-skazve.jpg',
+    price: 620,
+  },
+  {
+    id: 'TailsBook07',
+    category: 'forKids',
+    name: 'Русская народная сказка: "Петушок и меленка"',
+    poster:
+      'https://mnogo-skazok.ru/wp-content/uploads/petushok-zolotoj-grebeshok-i-jernovci.jpg',
+    price: 280,
+  },
+  {
+    id: 'TailsBook08',
+    category: 'forKids',
+    name: 'Русская народная сказка: "Зимовье зверей"',
+    poster:
+      'https://i0.wp.com/ihappymama.ru/iq/wp-content/uploads/2018/04/Skazka-Zimove-zverej.-Russkaya-narodnaya-skazka.png?resize=680%2C425&ssl=1',
+    price: 335,
+  },
+  {
+    id: 'TailsBook09',
+    category: 'forKids',
+    name: 'Русская народная сказка: "Сивка-бурка"',
+    poster:
+      'https://mnogo-skazok.ru/wp-content/uploads/russkaya-narodnaya-skazka-sivka-burka.jpg',
+    price: 415,
+  },
+  {
+    id: 'TailsBook10',
+    category: 'forKids',
+    name: 'Русская народная сказка: "Лиса и лапоть"',
+    poster:
+      'https://сезоны-года.рф/sites/default/files/images/detym/skazka_pro_zimu_6.jpg',
+    price: 235,
+  },
+  {
+    id: 'TailsBook11',
+    category: 'forKids',
+    name: 'Русская народная сказка: "Волк и семеро козлят"',
+    poster: 'https://kinderbox.ru/wp-content/uploads/2017/03/Volk-i-semero-kozlyat-1.jpg',
+    price: 380,
+  },
+  {
+    id: 'LiveHacker01',
+    category: 'autobiographies',
+    name: '16 вдохновляющих автобиографий. Лайфхакер',
+    poster:
+      'https://cdn.lifehacker.ru/wp-content/uploads/2017/08/12Bio_1502778538-1280x640.jpg',
+    price: 1280,
+  },
+  {
+    id: 'StevenKing01',
+    category: 'autobiographies',
+    name: 'Стивен Кинг: "Как писать книги". Автобиография писателя',
+    poster: 'https://flytothesky.ru/wp-content/uploads/2019/09/7-.jpg',
+    price: 1430,
+  },
+  {
+    id: 'ArnoldSchwarzenegger01',
+    category: 'autobiographies',
+    name: 'Арнольд Шварценеггер: "Вспомнить всё". Автобиография"',
+    poster:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIRgslQWISqFGfb0z3qbwRxIOdJydftJVu2A&usqp=CAU',
+    price: 1430,
   },
 ];
